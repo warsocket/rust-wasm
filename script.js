3a71d358bc780675e346d880e58f9107b999ba1a
Bram <warsocket@gmail.com>
2024-04-16 21:01:21 +0200

    Now with linear memory usage

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ async function main(){
 
 
 	//const importObject = { Math }; //needs unsafe in rust
-	const importObject = { Math: {random: () => Math.random()} };
+	// const memory = new WebAssembly.Memory({ //64k
+  	// 	initial: 16,
+  	// 	maximum: 16,
+	// })
+	// const view = new Uint8Array(memory.buffer)
+
+
+	// console.log(memory)
+	// console.log(memory.buffer)
+	// console.log(`view[0] = ${view[0]}`)
+
+	// const importObject = { js: {mem: memory}, Math: {random: () => Math.random()} };
+	const importObject = { Math: {random: () => Math.random()}, console };
 	const wasm = await WebAssembly.instantiateStreaming(resp, importObject)
+
 
 	console.log("add")
 	console.log(wasm.instance.exports.add(7, 3))
@@ -16,4 +29,17 @@ async function main(){
 
 	console.log("rnd")
 	console.log(wasm.instance.exports.rnd())
+
+	console.log("print")
+	wasm.instance.exports.print()
+
+
+	// let wasmMemory = new Uint8Array(wasm.instance.exports.memory.buffer);
+	// let ptr = wasm.instance.exports.get_ptr()
+	// console.log(wasmMemory[ptr])
+	// wasm.instance.exports.put(32)
+	// console.log(wasmMemory[ptr])
+
+
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 // No mangle an dexport name absically doe the same, as do extern and extern "C"
+use std::ffi::CString;
+use std::ffi::c_char;
+
+static mut mem:[u8;0xFF] = [42;0xFF];
 
 #[no_mangle]
 pub fn add(left: usize, right: usize) -> usize {
@@ -22,3 +26,38 @@ extern{
 pub extern fn rnd() -> usize {
     (unsafe{random()}*0xFFFFFF as f64).floor() as usize
 }    
+
+
+#[export_name = "get_ptr"]
+pub fn get_ptr() -> *const u8 {
+    unsafe { mem.as_ptr() }
+}
+
+
+#[export_name = "put"]
+pub fn put(b:u8) -> () {
+    unsafe{
+        mem[0] = b;
+    }
+}
+
+
+#[export_name = "print"]
+pub fn print() -> () {
+    unsafe{
+        log( "xxxzzz" );
+    }
+}
+
+
+#[link(wasm_import_module = "console")]
+extern "C" {
+    pub fn log(s:&str);
+}
+
+
+// #[link(wasm_import_module = "Console")]
+// extern{
+//     fn log(ptr:usize, len:usize) -> ();
+// }
+
